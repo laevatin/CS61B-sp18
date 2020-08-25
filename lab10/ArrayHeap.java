50006d209f5068802003070b773a6c563fa69b97
lizch <lizch@shanghaitech.edu.cn>
2020-08-25 12:24:35 +0800

    fixed removeMin

--- lab10/ArrayHeap.java
+++ lab10/ArrayHeap.java
@@ -169,11 +169,12 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
             return null;
         }
         contents[1].myItem = null;
-        size -= 1;
         if (size == 1) {
+            size -= 1;
             return item;
         }
         swap(1, size);
+        size -= 1;
         sink(1);
         return item;
     }
