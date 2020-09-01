c6087c0e70dbcc275c8f21a4499aa233d60fa1f8
lizch <lizch@shanghaitech.edu.cn>
2020-09-01 14:13:34 +0800

    hw11 finished

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -9,16 +9,52 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private boolean cycleFound = false;
+    private int[] from;
+    private int s;
 
     public MazeCycles(Maze m) {
         super(m);
+        s = maze.xyTo1D(1,1);
+        distTo[s] = 0;
+        from = new int[m.V()];
+        from[s] = s;
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        dfs(s);
     }
 
-    // Helper methods go here
+    private void dfs(int v) {
+        marked[v] = true;
+        announce();
+
+        for (int w : maze.adj(v)) {
+            if (cycleFound) {
+                return;
+            }
+
+            if (!marked[w]) {
+                from[w] = v;
+                dfs(w);
+            } else if (from[v] != w) {
+                cycleFound = true;
+                from[w] = v;
+
+                int cur = v;
+                edgeTo[cur] = from[cur];
+                while (cur != w) {
+                    cur = from[cur];
+                    edgeTo[cur] = from[cur];
+                }
+                announce();
+                return;
+            }
+
+        }
+
+    }
+
 }
 
